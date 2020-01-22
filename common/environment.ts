3c38350cded02cbf3048380694fe1b342f6e4466
Phablo Lisboa <phablolisboa01@gmail.com>
2020-01-21 23:33:35 -0300

    Teste SSH

--- common/environment.ts
+++ common/environment.ts
@@ -3,6 +3,7 @@ export const environment = {
         port: process.env.SERVER_PORT || 3000
     },
     database:{
-        url: process.env.DB_URL || 'mongodb://127.0.0.1/request-mango'
+        url: process.env.DB_URL || 'mongodb://127.0.0.1/request-mango',
+        lul:'xd'
     }
 }
